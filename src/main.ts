eb32d063c719a94c688bc126c526b8bd539b1a8d
mohid <mohid10587@gmail.com>
2025-11-28 22:28:01 +0600

    Re-add gitignore and remove large tracked files

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // your Next.js frontend URL
+    origin: ['http://localhost:3000', 'https://dawah2.vercel.app'], // your Next.js frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // if you want to send cookies
   });
